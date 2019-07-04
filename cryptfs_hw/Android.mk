b84878068fca37f90567c2daea0ab8087f9fe00f
David <davmcleod87@aol.com>
2019-07-04 13:35:08 -0400

    Update Android.mk

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -9,9 +9,14 @@ commonSharedLibraries := \
                         libcutils \
                         libutils \
                         libdl \
-                        libhardware
+                        libhardware \
+                        liblog
+
 commonIncludes := \
-                  hardware/libhardware/include/hardware/
+                  hardware/libhardware/include/hardware/ \
+                  $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_C_INCLUDES := $(commonIncludes)
 LOCAL_SRC_FILES := $(sourceFiles)
@@ -25,10 +30,19 @@ LOCAL_MODULE_OWNER := qcom
 ifeq ($(TARGET_SWV8_DISK_ENCRYPTION),true)
 LOCAL_CFLAGS += -DCONFIG_SWV8_DISK_ENCRYPTION
 endif
+
 # USE_ICE_FOR_STORAGE_ENCRYPTION would be true in future if
 # TARGET_USE_EMMC_USE_ICE is set
 ifeq ($(TARGET_USE_UFS_ICE),true)
 LOCAL_CFLAGS += -DUSE_ICE_FOR_STORAGE_ENCRYPTION
+endif
+
+ifeq ($(TARGET_LEGACY_HW_DISK_ENCRYPTION),true)
+LOCAL_CFLAGS += -DLEGACY_HW_DISK_ENCRYPTION
+endif
+
+ifeq ($(TARGET_KEYMASTER_WAIT_FOR_QSEE),true)
+LOCAL_CFLAGS += -DWAIT_FOR_QSEE
 endif
 
 include $(BUILD_SHARED_LIBRARY)
